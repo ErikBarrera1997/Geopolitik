1fcd51eca4549ddadb758adc5b5342fa27b151ed
Erik Barrera <erikbarrera376@gmail.com>
2026-06-14 19:00:26 -0600

    Proyect Base. SQLite and JavaFx configurations

---
+++ GEOPOLITIK/src/main/java/com/msservices/geopolitik/App.java
@@ -0,0 +1,24 @@
+package com.msservices.geopolitik;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
